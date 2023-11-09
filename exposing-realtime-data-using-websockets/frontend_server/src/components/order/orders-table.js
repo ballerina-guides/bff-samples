c82913e798f44ce570358037e0d91622c8b3dc82
Sasindu Alahakoon <dilsharasasindu@gmail.com>
2023-11-09 13:55:36 +0530

    Add license headers to frontend codes in websocket

--- exposing-realtime-data-using-websockets/frontend_server/src/components/order/orders-table.js
+++ exposing-realtime-data-using-websockets/frontend_server/src/components/order/orders-table.js
@@ -1,4 +1,21 @@
-import PropTypes from 'prop-types';
+/**
+ * Copyright (c) 2023, WSO2 LLC (http://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import {
   Box,
   Button,
@@ -12,9 +29,6 @@ import {
 import { Scrollbar } from 'src/components/scrollbar';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-// import useWebSocket, { ReadyState } from 'react-use-websocket';
-
-// const WebSocket = require('ws');
 
 export default function OrdersTable({items}) {
   const [location1, setLocation1] = useState({longitude: "...", latitude: "..."});
--- exposing-realtime-data-using-websockets/frontend_server/src/pages/index.js
+++ exposing-realtime-data-using-websockets/frontend_server/src/pages/index.js
@@ -1,7 +1,25 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC (http://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import Head from 'next/head';
 import * as React from 'react';
 import { Box, Button, Container, Stack, SvgIcon, Typography } from '@mui/material';
-import OrdersTable from 'src/sections/order/orders-table';
+import OrdersTable from 'src/components/order/orders-table';
 
 const Page = () => {
 
@@ -14,7 +32,7 @@ const Page = () => {
       <div>
         <Head>
           <title>
-            orders | MegaPort Kit
+            Orders | MegaPort Kit
           </title>
         </Head>
         <Box
